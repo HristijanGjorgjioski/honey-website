0d003bc38867fd95cf128a4255d9855b4deec13a
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-04-03 12:02:49 +0200

    Gallery section is almost done!

--- frontend/src/Home/Gallery/styles.js
+++ frontend/src/Home/Gallery/styles.js
@@ -2,6 +2,28 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
     container: {
-        marginTop: '10px'
+        marginTop: '10px',
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    cardMedia: {
+        width: '100%',
+        height: '400px',
+        filter: 'brightness(50%)'
+    },
+    text: {
+        position: 'absolute',
+        fontSize: '25px',
+        color: 'white',
+        transition: 'all .2s ease-in-out',
+        '&:hover': {
+            color: '#aab5ad',
+            transform: 'scale(1.1)'
+        },
+        [theme.breakpoints.down('xs')]: {
+            fontSize: '20px'
+        }
     }
 }))
