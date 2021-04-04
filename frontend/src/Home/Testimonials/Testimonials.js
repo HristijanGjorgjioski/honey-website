e2112a8851b5bb79f343e9ce9c0428a482f5321a
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-04-04 11:57:42 +0200

    Created 'Testimonials' section with photo slider

--- frontend/src/Home/Testimonials/Testimonials.js
+++ frontend/src/Home/Testimonials/Testimonials.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Carousel from 'react-material-ui-carousel'
-import { Container, Paper, Button, Typography, CardMedia } from '@material-ui/core';
+import { Container, Paper, Button, Typography, CardMedia, Grid } from '@material-ui/core';
 
 import useStyles from './styles';
 
@@ -16,27 +16,30 @@ const Testimonials = () => {
         {
             name: "Random Name #2",
             description: "Hello World!",
-            image: ''
+            image: 'https://cdn.pocket-lint.com/r/s/1200x/assets/images/142207-phones-feature-what-is-apple-face-id-and-how-does-it-work-image1-5d72kjh6lq.jpg'
         }
     ]
 
     const Item = ({ item }) => {
         return (
-            <Paper>
-                <CardMedia style={{ width: '200px', height: 'auto' }} component="img" image={item.image} />
-                <Typography>{item.name}</Typography>
-                <Typography>{item.description}</Typography>
-    
-                <Button className="CheckButton">
-                    Check it out!
-                </Button>
+            <Paper className={classes.paper}>
+                <Grid container>
+                    <Grid className={classes.grid1} item xs={12} sm={12}>
+                        <CardMedia style={{ width: '200px', height: '200px' }} component="img" image={item.image} />
+                        <Typography>{item.name}</Typography>
+                    </Grid>
+                    <Grid className={classes.grid1} item xs={12} sm={12}>
+                        <Typography>{item.description}</Typography>
+                    </Grid>
+                </Grid>
             </Paper>
         )
     }
 
     return (
         <Container className={classes.container}>
-            <Carousel>
+            <Typography className={classes.title}>Дел од нашите клиенти</Typography>
+            <Carousel className={classes.carousel}>
                 {items.map( (item, i) => <Item key={i} item={item} /> )}
             </Carousel>
         </Container>
--- frontend/src/Home/Testimonials/styles.js
+++ frontend/src/Home/Testimonials/styles.js
@@ -5,5 +5,38 @@ export default makeStyles((theme) => ({
         backgroundColor: '#dbd8ce',
         marginTop: '5%',
         marginBottom: '5%',
-    }
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    paper: {
+        width: '100%',
+        padding: '15px',
+    },
+    carousel: {
+        width: '100%',
+        padding: '15px',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    grid1: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    title: {
+        fontSize: '70px',
+        color: 'black',
+        transition: 'all .2s ease-in-out',
+        '&:hover': {
+            transform: 'scale(1.05)'
+        },
+        [theme.breakpoints.down('xs')]: {
+            fontSize: '30px'
+        }
+    },
 }))
