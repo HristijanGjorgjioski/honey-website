f5bd327626a796dfde1fb536320ba708bd8ff79e
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-04-07 09:40:02 +0200

    Started working on backend

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const CONNECTION_URL = process.env.CONNECTION_URL || 'mongodb+srv://honey-website:12345@cluster0.u2uu1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
