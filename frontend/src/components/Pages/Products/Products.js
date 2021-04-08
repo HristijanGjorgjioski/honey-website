678d58b5bf64ba56ecfc322b5bb00317476a5ab4
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-04-08 19:51:39 +0200

    Fixing some errors with Cart

--- frontend/src/components/Pages/Products/Products.js
+++ frontend/src/components/Pages/Products/Products.js
@@ -8,29 +8,22 @@ import { MainContext } from '../../../context/context';
 const Products = () => {
     const classes = useStyles();
     const { getProducts, products } = useContext(MainContext);
-    
+
     useEffect(() => {
         getProducts();
     }, [])
-
-
-    // const Products = products.allProducts.map((product, index) => {
-    //     return (
-            
-    //     )
-    // });
+    
+    console.log(products.allProducts);
 
     return (
         <Container className={classes.container}>
             <Typography className={classes.title} variant="h3">Продавница</Typography>
                 <Grid container justify="space-between" alignItems="stretch" spacing={2}>
-                    {/* {!products.allProducts.length ? <CircularProgress /> : (
-                        products.allProducts.map((product) => {
-                            <Grid key={product._id} item xs={6} sm={4}>
-                                <CustomCard photo={product.photo} title={product.title} price={product.price} description={product.description} weight={product.weight} id={product.id} quantity={product.quantity} />
-                            </Grid>
-                        })
-                    )} */}
+                    {products.allProducts ? products.allProducts.map((product) => {
+                        return (<Grid key={product._id} item xs={6} sm={4}>
+                            <CustomCard photo={product.photo} title={product.title} price={product.price} description={product.description} weight={product.weight} id={product.id} quantity={product.quantity} />
+                        </Grid>) 
+                    }) : "Нема производи"}
                 </Grid>
         </Container>
     )
