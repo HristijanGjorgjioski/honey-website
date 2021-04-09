3f3a080001af1a2392097ecc18aebbb2ebe69c76
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-04-09 07:09:30 +0200

    Successfully showing contact messages in admin panel and allowed admin to delete them

--- backend/controllers/contact-controller.js
+++ backend/controllers/contact-controller.js
@@ -1,5 +1,16 @@
 import Contact from '../models/contact-model.js';
 
+export const getContact = async (req, res) => {
+    try {
+        const allContacts = await Contact.find();
+        return res.render('all-contacts', {
+            contacts: allContacts
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 export const postContact = async (req, res) => {
     const { name, number, email, message } = req.body;
 
@@ -10,4 +21,11 @@ export const postContact = async (req, res) => {
     } catch (error) {
         console.log(error);
     }
-}
+};
+
+export const deleteContact = async (req, res) => {
+    const { _id } = req.params;
+
+    await Contact.deleteOne({ _id });
+    res.redirect('/contact');
+};
--- backend/routes/contact-router.js
+++ backend/routes/contact-router.js
@@ -1,9 +1,13 @@
 import express from 'express';
 
-import { postContact } from '../controllers/contact-controller.js';
+import { getContact, postContact, deleteContact } from '../controllers/contact-controller.js';
 
 const router = express.Router();
 
+router.get('/contact', getContact);
 router.post('/contact', postContact);
+
+router.get('/contacts/:_id', deleteContact);
+router.post('/contacts', deleteContact);
 
 export default router;
